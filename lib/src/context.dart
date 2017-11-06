72468ba8b670049a6a51d7f8a4e432c2d4aa5ada
thislooksfun <thislooksfun@repbot.org>
2017-11-06 04:11:15 -0600

    Start implementing dot resolution

--- lib/src/context.dart
+++ lib/src/context.dart
@@ -2,6 +2,7 @@ part of handlebars4dart;
 
 class _Context {
   _Context _parent = null;
+  Object _obj = null;
   Map<String, Object> _map;
   
   _Context(Map<String, Object> map) {
@@ -9,7 +10,22 @@ class _Context {
   }
   
   Object get(String name) {
-    Object o = _map[name] ?? _parent?.get(name);
-    return (o ?? '').toString();
+    Object out = null;
+    if (name == '.') {
+      out = _obj;
+    } else {
+      List<String> parts = name.split('.');
+      String first = parts.removeAt(0);
+      out = _map[first] ?? _parent?.get(first);
+      
+      for (Object p in parts) {
+        if (out is Map<String, Object>) {
+          out = out[p];
+        } else {
+          break;
+        }
+      }
+    }
+    return (out ?? '').toString();
   }
 }
