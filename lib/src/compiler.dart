1b6830645c8878653c08dd1e14828bcc6e9faf27
thislooksfun <thislooksfun@repbot.org>
2017-11-10 02:45:34 -0600

    Cleanup and document

--- lib/src/compiler.dart
+++ lib/src/compiler.dart
@@ -1,7 +1,8 @@
 part of compiled_mustache;
 
+/// Compile a string template into a [CompiledTemplate]
 CompiledTemplate compile(String tmplt) {
   _Parser p = new _Parser(tmplt);
   List<_Node> nodes = p.parse();
-  return new CompiledTemplate(nodes);
+  return new CompiledTemplate._internal(nodes);
 }
--- lib/src/errors.dart
+++
@@ -1,5 +0,0 @@
-part of compiled_mustache;
-
-// class HandlebarsError extends Error {
-//
-// }
