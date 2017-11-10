05e0b678abbf2196cda20efd95ec70f511354822
thislooksfun <thislooksfun@repbot.org>
2017-11-10 02:34:23 -0600

    Remove obsolete compile functions

--- lib/src/compiler.dart
+++ lib/src/compiler.dart
@@ -5,15 +5,3 @@ CompiledTemplate compile(String tmplt) {
   List<_Node> nodes = p.parse();
   return new CompiledTemplate(nodes);
 }
-
-CompiledTemplate compileWithPartials(String tmplt, Map<String, Object> partials) {
-  _Parser p = new _Parser(tmplt);
-  List<_Node> nodes = p.parse();
-  return new CompiledTemplate(nodes).compileWithPartials(partials);
-}
-
-CompiledTemplate compileWithPartialProvider(String tmplt, CompiledTemplate partialProvider(String name)) {
-  _Parser p = new _Parser(tmplt);
-  List<_Node> nodes = p.parse();
-  return new CompiledTemplate(nodes).compileWithPartialProvider(partialProvider);
-}
