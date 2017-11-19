a0a2d229b3a381baedbd213a1af9daffd835b7ff
thislooksfun <thislooksfun@repbot.org>
2017-11-19 01:09:56 -0600

    Start work on Travis integration

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+
+set -ev
+
+git clone https://github.com/thislooksfun/compiled_mustache.wiki.git
+grind doc_benchmark_wiki
+cd compiled_mustache.wiki
+git add -A
+git commit -m "Update Benchmarks"
+git push
+
+# pub publish --dry-run
---
+++ scripts/install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+set -ev
+
+chmod +x scripts/build.sh
+chmod +x scripts/test.sh
